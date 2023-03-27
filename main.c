e43a199f403fb75e1abe5efd4694180d0ab1387a
KhingSIMEO <Kingsimo@gmail.com>
2023-03-27 12:21:38 -0700

    updated

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - Entry point
  *
@@ -12,6 +13,7 @@ int len;
 int len2;
 unsigned int ui;
 void *addr;
+
 len = _printf("Let's try to printf a simple sentence.\n");
 len2 = printf("Let's try to printf a simple sentence.\n");
 ui = (unsigned int)INT_MAX + 1024;
